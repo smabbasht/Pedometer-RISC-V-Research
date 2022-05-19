8b9a8324d424b57b7aeb02ec06a93c5ef0146bd6
abbastaqvi <79911677+smabbasht@users.noreply.github.com>
2022-05-19 11:41:29 +0500

    pedometer initComplete

--- Verilog Files/pedometer.v
+++ Verilog Files/pedometer.v
@@ -6,31 +6,40 @@
 `include "writeBack.v"
 
 module pedometer(
-    clk, reset, countSteps, updateWeights, dualUpdateWeights, A, B, Addr1, Addr2, Data1, Data2, 
+    clk, reset, countSteps, updateWeight, dualUpdateWeights, A, B, Addr1, Addr2, Data1, Data2, 
 );
     input  clk;
     input  reset;
     input  countSteps;
-    input  updateWeights;
+    input  updateWeight;
     input  dualUpdateWeights;
     input  [7 :0] A, B; 
     input  [2 :0] Addr1, Addr2;
     input  [7 :0] Data1, Data2;
 
+    wire step;
     wire reset;
     wire countSteps;
+    wire updateWeight;
     wire updatedSteps;
+    wire updateTotalSteps;
     wire dualUpdateWeights;
-    wire [31:0] instruction;
     wire [ 3:0] opcode;
-    wire [ 7:0] A, B, Data1, Data2;
+    wire [31:0] instruction;
     wire [ 2:0] Addr1, Addr2;
+    wire [ 7:0] A, B, Data1, Data2, totalSteps, updatedTotalSteps;
+    wire [ 7:0] theta1, theta2, beta1, beta2, alpha1, alpha2;
 
     instrFetch IF(.clk(clk), .reset(reset), .countSteps(countSteps), .updateWeights(updateWeights), .dualUpdateWeights(dualUpdateWeights), .Addr1(Addr1), .Data1(Data1), .Addr2(Addr2), .Data2(Data2), .A(A), .B(B), .instruction(instruction));
 
     instrDecode ID(.instruction(instruction), .A(A), .B(B), .Addr1(Addr1), .Data1(Data1), .Addr2(Addr2), .Data2(Data2), .opcode(opcode));
 
-    regFile rFile(.clk(clk), .reset(reset), .updateWeight(updateWeight), .());
-    
+    controlUnit CU(.opcode(opcode), .updateWeight(updateWeight), .updateTotalSteps(updateTotalSteps), .reset(reset));
 
+    regFile rFile(.clk(clk), .reset(reset), .updateWeight(updateWeight), .updateTotalSteps(updateTotalSteps), .totalSteps(totalSteps), .Addr1(Addr1), .Data1(Data1), .Addr2(Addr2), .Data2(Data2), 
+    .theta1(theta1), .theta2(theta2), .beta1(beta1), .beta2(beta2), .alpha1(alpha1), .alpha2(alpha2));
+
+    ExBlock EX(.clk(clk), .A(A), .B(B), .step(step), .theta1(theta1), .theta2(theta2), .beta1(beta1), .beta2(beta2), .alpha1(alpha1), .alpha2(alpha2));
+
+    writeBack WB(.step(step), .writeTotalSteps(writeTotalSteps), .totalSteps(totalSteps), .updatedtotalSteps(updatedtotalSteps));
 endmodule
