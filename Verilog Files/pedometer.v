6ff47fdec3f5255e1366c13928b03824e16b9eac
abbastaqvi <79911677+smabbasht@users.noreply.github.com>
2022-05-19 10:20:27 +0500

    pedometer.v initial commit

--- Verilog Files/pedometer.v
+++ Verilog Files/pedometer.v
@@ -1,6 +1,23 @@
-module pedometer(
+`include "ExBlock.v"
+`include "controlUnit.v"
+`include "instrDecode.v"
+`include "instrFetch.v"
+`include "regFile.v"
+`include "writeBack.v"
 
+module pedometer(
+    clk, reset, countSteps, updateWeights, dualUpdateWeights, A, B, Addr1, Addr2, Data1, Data2, 
 );
+    input  clk;
+    input  reset;
+    input  countSteps;
+    input  updateWeights;
+    input  dualUpdateWeights;
+    input  [7 :0] A, B; 
+    input  [2 :0] Addr1, Addr2;
+    input  [7 :0] Data1, Data2;
+
     
     
+
 endmodule
